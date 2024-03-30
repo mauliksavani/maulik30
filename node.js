d229709cf9167d4bb65a5ce30870d1c5aa858902
dhruv <dhruv@gmail.com>
2024-03-30 15:55:52 +0530

    change_code

---
+++ node.js
@@ -0,0 +1,24 @@
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(400).send({
+            code: 400,
+            message: "that username and password are required"
+        });
+    }
+    const user = users.find(user => user.username === username && user.password === password);
+    if (!user) {
+        res.status(400).send({
+            code: 400,
+            message: "Invalid credentials",
+            data: {}
+        });
+    } else {
+        res.status(200).send({
+            code: 200,
+            message: "Login successful",
+            data: userusunsuhjwknmwkk
+        });
+    }
+});
+
